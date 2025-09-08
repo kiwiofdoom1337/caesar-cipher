c23be35b4a19cedf7e1edf5804182ea72a12977e
kiwiofdoom1337 <186161940+kiwiofdoom1337@users.noreply.github.com>
2025-09-08 13:49:46 +0300

    add rubocop and edit

--- cipher.rb
+++ cipher.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_num)
-  coded_string = Array.new()
-  string.split('').each do |el|
-    if ("a".."z").include?(el) || ("A".."Z").include?(el) || ("0".."9").include?(el)
-        if ("A".."Z").include?(el)
-          coded_string.push(((el.to_i(36) - 10 + shift_num) % 26 + 10).to_s(36).upcase)
-        elsif ("0".."9").include?(el)
-          coded_string.push(((el.to_i(36) + shift_num) % 10).to_s(36))
-        else
-          coded_string.push(((el.to_i(36) - 10 + shift_num) % 26 + 10).to_s(36))
-        end
+  coded_string = []
+  string.chars.each do |el|
+    if ('a'..'z').cover?(el) || ('A'..'Z').cover?(el) || ('0'..'9').cover?(el)
+      if ('A'..'Z').cover?(el)
+        coded_string.push((((el.to_i(36) - 10 + shift_num) % 26) + 10).to_s(36).upcase)
+      elsif ('0'..'9').cover?(el)
+        coded_string.push(((el.to_i(36) + shift_num) % 10).to_s(36))
+      else
+        coded_string.push((((el.to_i(36) - 10 + shift_num) % 26) + 10).to_s(36))
+      end
     else
-      coded_string.push(el) 
+      coded_string.push(el)
     end
   end
   puts coded_string.join
 end
 
-puts "Please enter a string you wish to code."
+puts 'Please enter a string you wish to code.'
 string = gets.chomp
-puts "By how many positions should it be shifted right?"
+puts 'By how many positions should it be shifted right?'
 shift = gets.chomp
-while !shift.split("").all?("0".."9")
-  puts "Please input a valid number."
+until shift.chars.all?('0'..'9')
+  puts 'Please input a valid number.'
   shift = gets.chomp
 end
 
